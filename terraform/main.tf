47328fc29da3a2a3f7fb2e4e531a37665699b73e
Sindre <sindre.demetrio@gmail.com>
2026-08-13 15:56:34 +0200

    feat: add container app env

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,36 @@
 locals {
-  project_name        = "container-scale-lab"
-  resource_group_name = "rg-${local.project_name}-${var.environment}"
-}
+  project_name                   = "container-scale-lab"
+  resource_group_name            = "rg-${local.project_name}-${var.environment}"
+  log_analytics_name             = "log-${local.project_name}-${var.environment}"
+  container_app_environment_name = "cae-${local.project_name}-${var.environment}"
 
-resource "azurerm_resource_group" "main" {
-  name     = local.resource_group_name
-  location = var.location
-
-  tags = {
+  common_tags = {
     environment = var.environment
     managed_by  = "terraform"
     project     = local.project_name
   }
 }
 
+resource "azurerm_resource_group" "main" {
+  name     = local.resource_group_name
+  location = var.location
+  tags     = local.common_tags
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = local.log_analytics_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.common_tags
+}
+
+resource "azurerm_container_app_environment" "main" {
+  name                       = local.container_app_environment_name
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  logs_destination           = "log-analytics"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+  tags                       = local.common_tags
+}
