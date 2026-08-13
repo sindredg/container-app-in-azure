fcf6733dd8fd5b7b4c19d054ce3cba291018db54
Sindre <sindre.demetrio@gmail.com>
2026-08-13 19:21:27 +0200

    feat: move terraform state to azure storage

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,16 @@
 terraform {
   required_version = ">= 1.6.0, < 2.0.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-container-scale-lab-tfstate"
+    storage_account_name = "stcslabsindredgtf"
+    container_name       = "tfstate"
+    key                  = "platform/dev.tfstate"
+
+    use_azuread_auth = true
+    use_cli          = true
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
