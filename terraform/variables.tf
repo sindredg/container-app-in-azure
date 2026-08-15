84dd5896119ed4cb4e6db9c2bf3959bc13cfe749
Sindre Grytebust <sindre.demetrio@gmail.com>
2026-08-15 20:00:14 +0200

    feat: add api-container-app.tf, edit outputs.tf, variables.tf, web-container-app.tf (#22)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,5 +24,11 @@ variable "container_registry_name" {
 variable "web_image_tag" {
   description = "Immutable version tag of the web image stored in ACR."
   type        = string
-  default     = "0.1.2"
+  default     = "0.2.0"
+}
+
+variable "api_image_tag" {
+  description = "Immutable version tag of the API image stored in ACR."
+  type        = string
+  default     = "0.1.0"
 }
